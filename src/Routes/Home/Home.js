c14ffa8e1152b87477e7027a159af70fe1cc7e62
rushikesh39 <123180691+rushikesh39@users.noreply.github.com>
2023-10-30 18:40:09 +0530

    Update Home.js

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
 import jwtDecode from "jwt-decode";
