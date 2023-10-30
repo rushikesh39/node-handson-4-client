f72f6058e2f86917d04ccf1a069fedab371ad271
rushikesh39 <123180691+rushikesh39@users.noreply.github.com>
2023-10-30 18:42:19 +0530

    Update Dashboard.js

--- src/Routes/Dashboard/Dashboard.js
+++ src/Routes/Dashboard/Dashboard.js
@@ -1,48 +1,48 @@
-import React, { useEffect } from 'react';
-import jwt_decode from 'jwt-decode';
-import { useNavigate } from 'react-router-dom';
+// import React, { useEffect } from 'react';
+// import jwt_decode from 'jwt-decode';
+// import { useNavigate } from 'react-router-dom';
 
-function Dashboard() {
-  const navi = useNavigate();
+// function Dashboard() {
+//   const navi = useNavigate();
 
-  const decodeToken = (token) => {
-    const decodedToken = jwt_decode(token);
-    return decodedToken;
-  };
+//   const decodeToken = (token) => {
+//     const decodedToken = jwt_decode(token);
+//     return decodedToken;
+//   };
 
-  const isTokenExpired = (exp) => {
-    const currentTime = Date.now() / 1000; // Convert to seconds
-    console.log("Current time:", currentTime);
-    console.log("Token expiration time:", exp);
-    return exp < currentTime;
-  };
+//   const isTokenExpired = (exp) => {
+//     const currentTime = Date.now() / 1000; // Convert to seconds
+//     console.log("Current time:", currentTime);
+//     console.log("Token expiration time:", exp);
+//     return exp < currentTime;
+//   };
 
-  const checkTokenExpiration = () => {
-    const authToken = localStorage.getItem('authToken');
-    console.log("auth token", authToken);
-    if (authToken) {
-      const decodedToken = decodeToken(authToken);
-      if (isTokenExpired(decodedToken.exp)) {
-        localStorage.removeItem("authToken");
-        alert("Session has expired. Please login again.");
-        navi("/login");
-      }
-    }
-    else{
-      alert("session expire")
-      navi("/login");
-    }
-  };
-  useEffect(() => {
-    checkTokenExpiration();
-  }, []); // Empty dependency array ensures it runs only once on mount
+//   const checkTokenExpiration = () => {
+//     const authToken = localStorage.getItem('authToken');
+//     console.log("auth token", authToken);
+//     if (authToken) {
+//       const decodedToken = decodeToken(authToken);
+//       if (isTokenExpired(decodedToken.exp)) {
+//         localStorage.removeItem("authToken");
+//         alert("Session has expired. Please login again.");
+//         navi("/login");
+//       }
+//     }
+//     else{
+//       alert("session expire")
+//       navi("/login");
+//     }
+//   };
+//   useEffect(() => {
+//     checkTokenExpiration();
+//   }, []); // Empty dependency array ensures it runs only once on mount
 
   
-  return (
-    <div>
-      Welcome to Dashboard
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       Welcome to Dashboard
+//     </div>
+//   );
+// }
 
-export default Dashboard;
+// export default Dashboard;
